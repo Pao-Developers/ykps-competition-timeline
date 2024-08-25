f7ae980b4b2233cd451e68d6027cbf38b155fe86
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2024-08-25 15:49:09 +0800

    impr: Now using json for configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,8 @@
 import { api } from "./api/api";
+import { apiPort as port } from "./config.json";
 
 // The server will be hosted under the /api path
 // "/" is for the vue app
-
-const port = 3000;
 
 api.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
